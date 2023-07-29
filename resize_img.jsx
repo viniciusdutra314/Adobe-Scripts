b9b15c33deb8c68c506bced120f64c030296c7d4
Vinícius Dutra <dutravinisousa2@usp.br>
2023-07-29 15:56:12 -0300

    resize any image to 16to9
    Fill the background with a blured copy of the image, creates a composition with 2 seconds of zoom

---
+++ resize_img.jsx
@@ -0,0 +1,29 @@
+//importar imagem
+var img_path = File.openDialog("Selecione uma imagem");
+var img_nome=img_path.name.split(".")[0]
+//criando composição
+var compWidth = 1920;  var compHeight = 1080;
+var compDuration = 5; var compFrameRate = 30; 
+var compName = img_nome+"_resize"
+var myComp = app.project.items.addComp(compName, compWidth, compHeight, 1.0, compDuration, compFrameRate);
+var img = app.project.importFile(new ImportOptions(File(img_path)))
+var BgImg = myComp.layers.add(img)
+var FrontImg = BgImg.duplicate()
+//nomes da layers
+BgImg.name="Background"
+FrontImg.name="Image"
+//vamos agora calcular o fator de escala
+var sx=compWidth/BgImg.width; var sy=compHeight/BgImg.height
+S_bg=Math.max(sx,sy)
+S_fr=Math.min(sx,sy)
+//vamos aplicar-lo
+BgImg.property("Transform").property("Scale").setValue([S_bg,S_bg]*100)
+FrontImg.property("Transform").property("Scale").setValue([S_fr,S_fr]*100)
+//blur
+var blurEffect = BgImg.property("Effects").addProperty("Gaussian Blur")
+blurEffect.property("Blurriness").setValue(30)
+//zoom animation
+myComp.layers.precompose([1,2], "Zoom");
+var PreComp= myComp.layer("Zoom")
+PreComp.scale.setValueAtTime(0,[120,120])
+PreComp.scale.setValueAtTime(2,[100,100])
